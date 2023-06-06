03342a49cdf444aaf308f2a3b25d56dd2169b8a9
saurav-png <110325751+saurav-png@users.noreply.github.com>
2023-06-06 16:38:45 +0545

    add skeleton code for canvas and to request data from API

--- Freecodecamp_Certification/visualizeDataWithBarChart/script.js
+++ Freecodecamp_Certification/visualizeDataWithBarChart/script.js
@@ -1,2 +1,42 @@
-let url="https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
-let requestData=new XMLHttpRequest();
+// d3
+    const sizeOfSVG={
+        width: 900,
+        height: 500
+    };
+
+    const padding = 60;
+
+    const title=() =>{
+        return d3.select('main')
+                .append('title')
+                .attr('id','title')
+                .text("GDP of United States")
+    };
+
+    let svg
+    const canvas=()=>{
+        const svg=d3.select('main')
+                    .append('svg')
+                    .attr('width',sizeOfSVG.width)
+                    .attr('height',sizeOfSVG.height)
+        return svg
+    };
+
+    const tooltip=()=>{
+        return d3.select('body')
+        .append('div')
+        .attr('id','tooltip')
+    };
+
+    let requestData=new XMLHttpRequest();
+    const sendRequest=()=>{
+        const url="https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+        requestData=(GET,url,true) //asynchronous data of boolean true
+    };
+
+    const heartOfChart=() => {
+        title();
+        svg=canvas();
+        tooltip();
+        requestData=sendRequest(requestData);
+    }
