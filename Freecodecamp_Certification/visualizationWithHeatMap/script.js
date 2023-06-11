e5e3345cc697d0d8a46bf365849af927bdc5bc7a
saurav-png <110325751+saurav-png@users.noreply.github.com>
2023-06-11 14:22:27 +0545

    add domain to scales, set data corresponding to x & y axis with color coding

--- Freecodecamp_Certification/visualizationWithHeatMap/script.js
+++ Freecodecamp_Certification/visualizationWithHeatMap/script.js
@@ -31,19 +31,71 @@
 // generate scales
     let xScale
     let yScale
+    let minYear
+    let maxYear
     scales=() =>{
+        minYear= d3.min(monthlyVariance,(d) =>{
+            return d.year
+        })
+        maxYear= d3.max(monthlyVariance,(d)=>{
+            return d.year
+        })
         xScale=d3.scaleLinear()
+                    .domain([minYear, maxYear])
                     .range([padding, sizeOfSVG.width -padding])
         yScale= d3.scaleTime()
+                    .domain([new Date(0, 0, 0, 0, 0, 0, 0), new Date(0, 12, 0, 0, 0, 0, 0)])
                     .range([padding, sizeOfSVG.height-padding])
     }
 
+// draw bars
+    drawBars= () =>{
+        svg.selectAll('rect')
+            .data(monthlyVariance)
+            .enter()
+                .append('rect')
+                .attr('class','cell')
+                .attr('fill',(d) =>{
+                    variance=d.variance
+                    if(variance <= -2){
+                        return 'SteelBlue'
+                    }else if(variance<= -1){
+                        return 'LightSteelBlue'
+                    }else if(variance < 1){
+                        return 'Orange'
+                    }else{
+                        return 'Crimson'
+                    }
+                })
+                .attr('data-year',(d) =>{
+                    return d.year
+                })
+                .attr('data-month',(d) =>{
+                    return d.month -1
+                })
+                .attr('data-temp',(d) =>{
+                    return baseTemperature + d.variance
+                })
+                .attr('height', (sizeOfSVG.height -(2 * padding)) / 12)
+                .attr('y', (d) =>{
+                    return yScale(new Date(0, d.month -1, 0, 0, 0, 0, 0))
+                })
+                .attr('width',(d) =>{
+                    let numberOfYears=maxYear - minYear
+                    return (sizeOfSVG.width - (2 * padding)) / numberOfYears
+                })
+                .attr('x',(d) =>{
+                    return xScale(d.year)
+                })
+    }
 
 // generate axes
 let svg
     axes=() =>{
         let xAxis= d3.axisBottom(xScale)
+                        .tickFormat(d3.format('d'))
         let yAxis= d3.axisLeft(yScale)
+        
         svg.append('g')
                 .call(xAxis)
                 .attr('id','x-axis')
@@ -71,6 +123,7 @@ let monthlyVariance=[]
             description(baseTemperature)
             svg=canvas()
             scales()
+            drawBars()
             axes()
         }
         requestData.send()
