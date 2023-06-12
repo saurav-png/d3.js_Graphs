1a670f80da1ddae9573fa9b12273923ad17e1390
saurav-png <110325751+saurav-png@users.noreply.github.com>
2023-06-12 12:38:09 +0545

    create page with skeleton code for choropleth map project

---
+++ Freecodecamp_Certification/visualizationWithChoropleth/script.js
@@ -0,0 +1,10 @@
+let USeducationURL="https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json"
+let UScountyURL="https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json"
+
+let countyData
+let educationData
+
+// draw the canvas to display svg
+const canvas=()=>{
+    
+}
