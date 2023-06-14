31c51411995a821d9a7af3ec8be22b2543f67ea8
saurav-png <110325751+saurav-png@users.noreply.github.com>
2023-06-14 14:19:41 +0545

    no schemeCategory20 function in d3 so created our own so that varied category have diff colors

--- Freecodecamp_Certification/visualizationWithTreemap/script.js
+++ Freecodecamp_Certification/visualizationWithTreemap/script.js
@@ -53,7 +53,26 @@ treeMapCreate=() =>{
 
     let colorScale = d3.scaleOrdinal()
                         .domain(uniqueCategories)
-                        .range(d3.schemeCategory10);
+                        .range(["#1f77b4",  //no schemeCategory20 so created our own
+                                "#aec7e8",
+                                "#ff7f0e",
+                                "#ffbb78",
+                                "#2ca02c",
+                                "#98df8a",
+                                "#d62728",
+                                "#ff9896",
+                                "#9467bd",
+                                "#c5b0d5",
+                                "#8c564b",
+                                "#c49c94",
+                                "#e377c2",
+                                "#f7b6d2",
+                                "#7f7f7f",
+                                "#c7c7c7",
+                                "#bcbd22",
+                                "#dbdb8d",
+                                "#17becf",
+                                "#9edae5"])
 
 
     let rects = canvas.selectAll('g')
